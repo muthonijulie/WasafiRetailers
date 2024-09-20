d84191883a4041b4a93cfe9bac5078390336197c
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-09-20 10:53:14 +0300

    Wasafi Retailers project setup

--- WasafiRetPr/settings.py
+++ WasafiRetPr/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'WasafiRetPr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
