0348c11139f0905fe1bde423cfcac6e31e6172d0
PhilomenaMusili <philomenamusili2@gmail.com>
2024-09-23 20:39:01 +0300

    Updated UI

--- WasafiRetPr/settings.py
+++ WasafiRetPr/settings.py
@@ -127,8 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-
-
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
