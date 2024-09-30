5bd9970b3cc5bb74e9c9d807e441ee4fe7b743d0
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-09-30 06:48:09 +0300

    Implementation of add to cart,cartpage and totals

--- WasafiRetPr/WasafiRet/urls.py
+++ WasafiRetPr/WasafiRet/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('category_create/',views.category_create,name='category_create'),
     path('',views.product_list,name='product_list'),
     path('product_create/', views.product_create, name='product_create'),
-    path('cart/', views.cart, name='cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/add/<int:product_id>',views.add_to_cart,name='add_to_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('detail/<int:product_id>/', views.detail, name='detail'),
 
