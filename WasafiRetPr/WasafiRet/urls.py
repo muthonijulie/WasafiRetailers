b6ce4d478815f006e6fb332985924e48fd09869b
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-09-30 22:36:52 +0300

    Remove and update cart item

--- WasafiRetPr/WasafiRet/urls.py
+++ WasafiRetPr/WasafiRet/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('product_create/', views.product_create, name='product_create'),
     path('cart/', views.view_cart, name='view_cart'),
     path('cart/add/<int:product_id>',views.add_to_cart,name='add_to_cart'),
+    path('cart/update/<int:cart_item_id>',views.update_cart,name='update_cart'),
+    path('cart/remove/<int:cart_item_id>',views.remove_cart,name='remove_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('detail/<int:product_id>/', views.detail, name='detail'),
 
