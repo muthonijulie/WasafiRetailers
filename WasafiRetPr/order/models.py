f3aae7dd12ceb05b6c31f9a6b4573cbe506862ad
PhilomenaMusili <philomenamusili2@gmail.com>
2024-10-07 05:57:45 +0300

    Updates

--- WasafiRetPr/order/models.py
+++ WasafiRetPr/order/models.py
@@ -13,18 +13,22 @@ class Order(models.Model):
    
     shipping_name = models.CharField(max_length=255)
     shipping_email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
     shipping_address = models.TextField()
     shipping_city = models.CharField(max_length=100)
     shipping_zip_code = models.CharField(max_length=20)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
+    def get_order_total(self):
+        return sum(item.get_total() for item in self.items.all())
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
-    def __str__(self):
-        return f"{self.quantity} of {self.product.name}"
+    def get_total(self):
+        return self.product.price * self.quantity
+
