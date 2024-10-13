8c213cd95f046ee09f88ff02a6a399e40bdb34e3
PhilomenaMusili <philomenamusili2@gmail.com>
2024-10-13 15:55:56 +0300

    Did Cancel order

--- WasafiRetPr/order/views.py
+++ WasafiRetPr/order/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages  
 from .models import Order, OrderItem
 from .forms import OrderForm
 from WasafiRet.models import Cart, CartItem
@@ -47,6 +48,21 @@ def order_confirmation(request, order_id):
         'order': order,
         'order_total': order_total, 
     })
+
+@login_required
+def cancel_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+ 
+    if order.status == 'Pending':
+        order.status = 'Canceled'
+        order.save()
+        
+        messages.success(request, "Your order has been canceled.")
+    else:
+       
+        messages.error(request, "This order cannot be canceled.")
+
+    return redirect('orders:order_history') 
 
 @login_required
 def order_history(request):
