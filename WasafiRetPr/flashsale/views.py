d979df28a70b1280484a84798b6ed407de3d59a8
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-10-20 06:50:20 +0300

    Worked on the flashsale app

--- WasafiRetPr/flashsale/views.py
+++ WasafiRetPr/flashsale/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
-from flashsale.models import Flashsale
+from .models import Flashsale
 
 
 # Create your views here.
 def flashsale(request):
     current=timezone.now()
-    print("Current Time:", current)
+    
     flashsale=Flashsale.objects.filter(start_time__lte=current,end_time__gte=current)
    
     return render(request, 'flashsale/flash.html', {'flashsale':flashsale})
