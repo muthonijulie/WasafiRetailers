cc8920745678e21b8cc30f0c784eec3e73bcb4c3
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-11-04 07:51:05 +0300

    edited the ft/payment urls.py

--- WasafiRetPr/payment/urls.py
+++ WasafiRetPr/payment/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('payment/initiate-stk-push/', views.initiate_stk_push, name='initiate-stk-push'),
-    
-=======
+
     # Add your URL patterns here
->>>>>>> master
 ]
+
